93f88e74fc9d0e3b632401c68543c73307c1b08c
Satwato <stwtdey@gmail.com>
2018-11-09 02:18:29 +0530

    6th commit to app

--- src/components/Jumbotron1.jsx
+++ src/components/Jumbotron1.jsx
@@ -6,7 +6,7 @@ class Jumbotron1 extends Component{
     return(
       <div className="jumbotron1 jumbotron-fluid">
         <div className="container">
-          <h1 className="display-3">a e s t h e t i c</h1>
+          <h1 className="display-5">a e s t h e t i c</h1>
           <p className="lead"> l o f i   a n d   c h i l l</p>
         </div>
       </div>
