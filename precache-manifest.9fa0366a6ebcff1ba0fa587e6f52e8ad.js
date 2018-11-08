3308e1fcb959f39ef15d54ea7974b96145abc698
Satwato <stwtdey@gmail.com>
2018-11-08 14:46:49 +0530

    Updates

---
+++ precache-manifest.9fa0366a6ebcff1ba0fa587e6f52e8ad.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "04e794069cb06289b5c609f3aaae9082",
+    "url": "/my-app/static/media/i3.04e79406.gif"
+  },
+  {
+    "revision": "628687ed70761a14f7077e179132856d",
+    "url": "/my-app/static/media/i2.628687ed.gif"
+  },
+  {
+    "revision": "137a838dc81b6f6f73538588b5c93529",
+    "url": "/my-app/static/media/i1.137a838d.gif"
+  },
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "f686b798f6c138160f86",
+    "url": "/my-app/static/js/main.f686b798.chunk.js"
+  },
+  {
+    "revision": "e1def844c98367c903ae",
+    "url": "/my-app/static/js/1.e1def844.chunk.js"
+  },
+  {
+    "revision": "f686b798f6c138160f86",
+    "url": "/my-app/static/css/main.581c56ac.chunk.css"
+  },
+  {
+    "revision": "a25bcfbc7089518ead0892c969e3c105",
+    "url": "/my-app/index.html"
+  }
+];
